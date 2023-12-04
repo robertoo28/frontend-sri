c51362cfe11e213ac319cffb2a5dcf5d1645aff5
Roberto Alvarado <roberto.alvarado@udla.edu.ec>
2023-12-04 08:11:46 -0500

    Se cambio nuevamente los puertos. Si quiere usar contenedores unicamente cambie los puertos de la clase ClientService.js sino deje los que están por defecto

--- src/services/ClientService.js
+++ src/services/ClientService.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 const CLIENT_ES_CONTRIBUYENTE_REST_API_URL =
-  "http://localhost:3001/api/existePorNumeroRuc?numeroRuc=";
+  "http://localhost:8080/api/existePorNumeroRuc?numeroRuc=";
 
 const CLIENT_PUNTOS_LICENCIA_REST_API_URL =
-  "http://localhost:3002/api/prueba?ps_tipo_identificacion=CED&ps_identificacion=";
+  "http://localhost:8081/api/prueba?ps_tipo_identificacion=CED&ps_identificacion=";
 
 class ClientService {
   esContribuyente(cedula) {
